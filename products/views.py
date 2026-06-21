80306c77f76b72686c28917e2f08333150354979
LucasyOliver <lucas.costa.oliveira000@gmail.com>
2026-06-21 18:50:03 -0300

    Add Brand List Pagination

--- products/views.py
+++ products/views.py
@@ -8,6 +8,7 @@ class BrandListView(ListView):
     model = Brand
     template_name = 'brands_list.html'
     context_object_name = 'brands'
+    paginate_by = 10
 
     def get_queryset(self):
         queryset = super().get_queryset()
